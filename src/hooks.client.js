762cd2f12f1e07642a95e6a6aa18baac1d6c02d7
Emil Zackrisson <emil.zackrisson@gmail.com>
2023-06-26 18:55:52 +0200

    Added enviroment

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -14,7 +14,9 @@ Sentry.init({
 	replaysOnErrorSampleRate: 1.0,
 
 	// If you don't want to use Session Replay, just remove the line below:
-	integrations: [new Replay()]
+	integrations: [new Replay()],
+
+	environment: import.meta.env.MODE,
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
